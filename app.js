4c12998f13880c6266a1103a61e8123e38601b91
Robin <yb6925@naver.com>
2022-11-07 23:55:59 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const signUp = document.querySelector(".sign-up");
+signUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("hi");
+});
+
+//로그인 구현
+//localStorage에서 꺼내오자
+
+//회원가입 구현
+//나는 솔직히 pop-up이 떠서 구현했으면 좋겠어
