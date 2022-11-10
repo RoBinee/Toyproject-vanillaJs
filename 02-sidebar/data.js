c542c13721b019604bb9cb92d0810739a741c9ac
Robin <yb6925@naver.com>
2022-11-10 23:20:23 +0900

    second commit

---
+++ 02-sidebar/data.js
@@ -0,0 +1,56 @@
+// const sublinks = [
+//   {
+//     page: "products",
+//     links: [
+//       { label: "payment", icon: "fas fa-credit-card", url: "products.html" },
+//       { label: "terminal", icon: "fas fa-credit-card", url: "products.html" },
+//       { label: "connect", icon: "fas fa-credit-card", url: "products.html" },
+//     ],
+//   },
+//   {
+//     page: "developers",
+//     links: [
+//       { label: "plugins", icon: "fas fa-book", url: "products.html" },
+//       { label: "libraries", icon: "fas fa-book", url: "products.html" },
+//       { label: "plugins", icon: "fas fa-book", url: "products.html" },
+//       { label: "billing", icon: "fas fa-book", url: "products.html" },
+//     ],
+//   },
+//   {
+//     page: "company",
+//     links: [
+//       { label: "about", icon: "fas fa-briefcase", url: "products.html" },
+//       { label: "customers", icon: "fas fa-briefcase", url: "products.html" },
+//     ],
+//   },
+// ];
+
+//array links -> 각 원소는 object,
+const links = [
+  {
+    category: "analtytics",
+    links: [
+      { label: "dashboard", icon: "fa-solid fa-table-columns" },
+      { label: "perfomance", icon: "fa-solid fa-chart-pie" },
+    ],
+  },
+  {
+    category: "content",
+    links: [
+      { label: "guides", icon: "fa-regular fa-file-lines" },
+      { label: "hotspots", icon: "fa-solid fa-circle-notch" },
+      { label: "checklists", icon: "fa-regular fa-circle-check" },
+      { label: "nps", icon: "fa-regular fa-thumbs-up" },
+    ],
+  },
+  {
+    category: "customization",
+    links: [
+      { label: "segments", icon: "fa-regular fa-clone" },
+      { label: "themes", icon: "fa-regular fa-pen-to-square" },
+    ],
+  },
+];
+
+//user
+//<i class="fa-regular fa-circle-user"></i>
