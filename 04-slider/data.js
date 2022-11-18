8a7be6ee7b498dbf4cd77e91f68e48e731999667
Robin <yb6925@naver.com>
2022-11-18 23:46:52 +0900

    plan commit

---
+++ 04-slider/data.js
@@ -0,0 +1,22 @@
+const datas = [
+  {
+    title: "spring",
+    img: "https://cdn.pixabay.com/photo/2022/05/03/23/35/rapeseed-7172836_960_720.jpg",
+  },
+  {
+    title: "summer",
+    img: "https://cdn.pixabay.com/photo/2017/06/17/18/35/beach-2413081_960_720.jpg",
+  },
+  {
+    title: "autumn",
+    img: "https://cdn.pixabay.com/photo/2017/01/30/15/35/autumn-2021154_960_720.jpg",
+  },
+  {
+    title: "winter",
+    img: "https://cdn.pixabay.com/photo/2019/12/29/13/59/trees-4727156_960_720.jpg",
+  },
+  {
+    title: "christmas",
+    img: "https://cdn.pixabay.com/photo/2021/12/26/19/36/christmas-tree-6895765_960_720.jpg",
+  },
+];
